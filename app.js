1fdd893fe757fd8e54cebaf6fc50f89df315251d
mwarnsley <marcus.warnsley@gmail.com>
2016-03-03 16:31:35 -0500

    noramlizing the endings

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-var express = require('express')
+var express = require('express');
 
-var app = express()
-var port = 5000
+var app = express();
+var port = 5000;
+
 
 app.get('/', function( req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
-})
+    res.sendFile(__dirname + '/dist/index.html');
+});
 
 app.get('/index.css', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.css')
-})
+    res.sendFile(__dirname + '/dist/index.css');
+});
 
 app.get('/index.js', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.js')
-})
+    res.sendFile(__dirname + '/dist/index.js');
+});
 
 app.listen(port, function () {
-    console.log("Express server on port " + port)
-})
+    console.log("Express server on port " + port);
+});
